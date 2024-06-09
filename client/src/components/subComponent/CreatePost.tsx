b096ad4ccabf046d1333203e90cde9fc69d13f55
Zobayer <mdzobayerhossen520@gmail.com>
2024-06-09 21:23:50 +0600

    nothing new

--- client/src/components/subComponent/CreatePost.tsx
+++ client/src/components/subComponent/CreatePost.tsx
@@ -1,10 +1,19 @@
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContex } from "../../lib/Reducher";
 import { FaFileImage } from "react-icons/fa6";
+import { GrLinkPrevious } from "react-icons/gr";
+import { GrLinkNext } from "react-icons/gr";
+import ImageLoder from './../../lib/ImageLoder';
+import { ImCancelCircle } from "react-icons/im";
 const CreatePost = () => {
-    const { state } = useContext(AppContex);
-
+    const { state ,dispach} = useContext(AppContex);
+  const [create, setCreate] = useState({photo:"",next:""});
+  console.log(create.next ,create.photo);
+  const ImageViews =async(file:File) => {
+    const ImageConvert:any = await ImageLoder(file);
+    setCreate({photo:ImageConvert});
+  }
   if(!state.postData) return null
     
     return (
@@ -12,14 +21,60 @@ const CreatePost = () => {
         <div className="mainContuner">
           <div className="header">
             <p>Create a new post</p>
+            <ImCancelCircle
+              className="cancile"
+              onClick={() => {
+                dispach({ type: "POSTDATA", value: false });
+                setCreate({ photo: "", next: "" });
+              }}
+            />
           </div>
           <hr />
           <div className="mainBody">
-                    <FaFileImage className="IMG" />
-                    <p>Choose your won photo</p>
-                    <button>
-                        <input type="file" accept=".jpeg , .png , .jpg" />
-                        Select your photo</button>
+            {create.photo ? (
+             
+                create.next ? (
+                <>
+                  <div className="postnow">
+                    <textarea name="post" id="post" autoFocus></textarea>
+                    <button className="btnPost">post now</button>
+                  </div>
+                </>
+                ):(
+                <>
+                  <div className="boundary">
+                    <img src={create.photo} alt="" />
+                    <div className="towbtn">
+                      <GrLinkPrevious
+                        className="pre"
+                        onClick={() => setCreate({ photo: "" })}
+                      />
+                      <GrLinkNext
+                        className="next"
+                          onClick={() => setCreate((pre) =>({
+                            ...pre,
+                            next:"true"
+                        }))}
+                      />
+                    </div>
+                  </div>
+                </>
+                )
+              
+            ) : (
+              <>
+                <FaFileImage className="IMG" />
+                <p>Choose your won photo</p>
+                <button>
+                  <input
+                    type="file"
+                    accept=".jpeg , .png , .jpg"
+                    onChange={(e) => ImageViews(e.target.files[0])}
+                  />
+                  Select your photo
+                </button>
+              </>
+            )}
           </div>
         </div>
       </div>
